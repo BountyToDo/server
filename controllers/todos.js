e56f3b2d48b801db7ee17bba3b729edca456c659
agrha <agrhakartz@gmail.com>
2018-03-18 08:05:44 +0700

    added model initiate express

---
+++ controllers/todos.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Types.ObjectId
+const Schema = mongoose.Schema
+
+const todoSchema = new Schema({
+    UserId:{
+        type:Schema.Types.ObjectId,
+        ref:'users'
+    },
+    body:{
+        type:String,
+        required : [true,'You Must Fill a todo']
+    },
+    status:{
+        type:String
+    },
+    timestamps:true
+})

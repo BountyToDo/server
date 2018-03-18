1eae11b6fe60eb54d12ff011f0123c42936e4c20
agrha <agrhakartz@gmail.com>
2018-03-18 08:11:06 +0700

    starting todo controller

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,20 +1,7 @@
-const mongoose = require('mongoose')
-const ObjectId = mongoose.Types.ObjectId
-const Schema = mongoose.Schema
+const Todos = require('../models/Todos')
 
-const todoSchema = new Schema({
-    UserId:{
-        type:Schema.Types.ObjectId,
-        ref:'users'
-    },
-    body:{
-        type:String,
-        required : [true,'You Must Fill a todo']
-    },
-    status:{
-        type:String
-    },
-    timestamps:true
-})
-
-module.exports = mongoose.model('todos',todoSchema)
+class Todos {
+    static viewAll(){
+        Todos.find()
+    }
+}
